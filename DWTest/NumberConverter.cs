8dad107e61a6d68ab52d25eecd107855fc242080
IvanZheleznyakov <iz14@yandex.ru>
2021-03-24 10:23:14 +0300

    init

---
+++ DWTest/NumberConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DWTest
+{
+    public static class NumberConverter
+    {
+        public static int RomanToInt(string romanNumber)
+        {
+            return 0;
+        }
+    }
+}
